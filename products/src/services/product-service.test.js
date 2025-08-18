22a243aaafa8b185cfc48a4ba97c8d0df2e1637e
Priyank-1 <singhumang444@gmail.com>
2025-08-18 20:22:26 +0530

    feat: 124: new-feat

--- products/src/services/product-service.test.js
+++ products/src/services/product-service.test.js
@@ -1,6 +1,6 @@
 // which sertvice it is
 describe("ProductService",()=>{
-    // which function it is
+    // which function it is and what are we testing
     describe("CreateProduct",()=>{
         // what it should do / Scenarios to be tested
         test("validate user inputs",()=>{});
